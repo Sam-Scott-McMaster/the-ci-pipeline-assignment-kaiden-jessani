57dd5922519880781f6a53de1df485ca715a3e1d
kaiden-jessani <jessanik@mcmaster.ca>
2024-10-01 18:44:57 -0400

    Delete excess_files

--- 2XC3-labs/week1/Lab_1.1_Question_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Please enter a directory to remove all the .tmp files from"
-read directory
-cd $directory
-ls $directory
-rm *.tmp
-echo "All .tmp files have been removed :)"
--- 2XC3-labs/week3/greet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Hello $1 $3! May I call you $2?"
